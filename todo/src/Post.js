7cf7f5395a9cb8bc7aea335e5d9b908de59ff178
gabesharpton <gabriel.sharpton@gmail.com>
2019-12-22 20:23:30 -0600

    inline style jsx

--- todo/src/Post.js
+++ todo/src/Post.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 export default function Post () {
+    // let styles = {
+    //     backgroundColor: 'green'
+    // }
   return (
     <div className="Post">
-      <h1>This is a Post</h1>
+      <h1 style={{backgroundColor: 'green'}}>This is a Post</h1>
+      <h1 style={{backgroundColor: 'red'}}>This is post 2</h1>
     </div>
   );
 }
